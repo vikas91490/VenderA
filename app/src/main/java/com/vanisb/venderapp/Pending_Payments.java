1e6c3d0291c6f3ba11cfbcc10f80d7b8dd0bd52e
vikas <vikas91490@gmail.com>
2018-12-18 22:53:11 +0530

    last update

---
+++ app/src/main/java/com/vanisb/venderapp/Pending_Payments.java
@@ -0,0 +1,13 @@
+package com.vanisb.venderapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pending_Payments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.pending__payments );
+    }
+}
